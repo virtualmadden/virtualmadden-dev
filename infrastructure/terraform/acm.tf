1617caf6d6043ad5b844a33fc5d11e16abf92aa6
Jonathan Madden <virtualmadden@users.noreply.github.com>
2019-12-20 16:40:28 -0800

    Update acm.tf

--- infrastructure/terraform/acm.tf
+++ infrastructure/terraform/acm.tf
@@ -23,6 +23,7 @@ resource "aws_route53_record" "cert_validation" {
     "${aws_acm_certificate.cert.domain_validation_options.0.resource_record_value}"
   ]
   ttl     = 60
+  allow_overwrite = true
 }
 
 resource "aws_acm_certificate_validation" "cert" {
