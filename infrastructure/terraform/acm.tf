d7bc2e3cdb98b238d7f52882fba73e21a8b978e8
Jonathan Madden <virtualmadden@users.noreply.github.com>
2019-12-20 16:05:31 -0800

    Update acm.tf

--- infrastructure/terraform/acm.tf
+++ infrastructure/terraform/acm.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 resource "aws_acm_certificate" "cert" {
   domain_name               = "${var.domain}"
-  subject_alternative_names = "www.${var.domain}"
+  subject_alternative_names = ["www.${var.domain}"]
   validation_method         = "DNS"
 
   provider = "aws.certs"
