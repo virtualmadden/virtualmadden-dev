81ef130769feed9b5c3d7352b5cc0ad67c1e5728
Jonathan Madden <virtualmadden@users.noreply.github.com>
2019-12-20 17:27:59 -0800

    Update acm.tf

--- infrastructure/terraform/acm.tf
+++ infrastructure/terraform/acm.tf
@@ -26,10 +26,23 @@ resource "aws_route53_record" "cert_validation" {
   allow_overwrite = true
 }
 
+resource "aws_route53_record" "cert_validation_alt" {
+  name    = "${aws_acm_certificate.cert.domain_validation_options.1.resource_record_name}"
+  type    = "${aws_acm_certificate.cert.domain_validation_options.1.resource_record_type}"
+  zone_id = "${aws_route53_zone.origin.zone_id}"
+  records = [
+    "${aws_acm_certificate.cert.domain_validation_options.1.resource_record_value}"
+  ]
+  ttl     = 60
+  allow_overwrite = true
+}
+
 resource "aws_acm_certificate_validation" "cert" {
   certificate_arn         = "${aws_acm_certificate.cert.arn}"
+
   validation_record_fqdns = [
-    "${aws_route53_record.cert_validation.fqdn}"
+    "${aws_route53_record.cert_validation.fqdn}",
+    "${aws_route53_record.cert_validation_alt.fqdn}"
   ]
 
   provider = "aws.certs"
